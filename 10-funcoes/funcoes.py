e2105af05d04904e87975ba947730293057cf574
Lui Gill Aquini <lgaquini@gmail.com>
2025-01-28 23:04:51 -0300

    Começo aula 5

---
+++ 10-funcoes/funcoes.py
@@ -0,0 +1,9 @@
+# Funções são trechos reutilizáveis de código que resolvem um problema específico.
+
+# parâmetros são valores esperados pela função para executar
+def soma_numeros(a, b):
+    return a + b
+
+
+# argumentos são passados como parâmentros da função
+print(soma_numeros(10, 20))
